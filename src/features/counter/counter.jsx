1d3836e9bf7a8e71026a93ea07708b6ac5053fd8
(hyebin) <(sksp4334@naver.com)>
2021-04-27 14:19:53 +0900

    add counter.js

---
+++ src/features/counter/counter.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { increment, decrement } from "./counterSlice";
+
+const Counter = (props) => {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  const clickIncrement = () => {
+    dispatch(increment());
+  };
+  const clickDecrement = () => {
+    dispatch(decrement());
+  };
+  return (
+    <div>
+      <div>
+        <button aria-label="Increment value" onClick={clickIncrement}>
+          +
+        </button>
+        <button aria-label="Decrement value" onClick={clickDecrement}>
+          -
+        </button>
+        <hr />
+        <span>{count}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
